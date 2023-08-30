48640bc647349a1ea0b0d0cdb3a5ef8e0d8bbd92
KamilSliwka <kamilsliwinski81@gmail.com>
2023-08-30 20:40:28 +0200

    the class Array2D responsible for storing the map

---
+++ Array2D.h
@@ -0,0 +1,53 @@
+#ifndef JAKDOJADE_ARRAY2D_H
+#define JAKDOJADE_ARRAY2D_H
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+class Array2D {
+    char** p;
+    int w = 0;
+    int h = 0;
+
+    Array2D(int ww, int hh) {
+
+        w = ww;
+        h = hh;
+        p = new char* [h];
+        for (int i = 0; i < h; i++) {
+            p[i] = new char[w];
+            memset(p[i], '.', w);
+        }
+    }
+
+    void Print() {
+        for (int i = 0; i < h; i++) {
+
+            for (int j = 0; j < w; j++) {
+                cout << p[i][j];
+            }
+            cout << endl;
+        }
+    }
+
+    char GetSignFromMap(int x, int y) {
+        if (x < 0 || y < 0 || y >= w || x >= h) {
+            return('.');
+        }
+        else {
+            return p[x][y];
+        }
+    }
+
+    ~Array2D() {
+        for (int i = 0; i < h; i++) {
+            delete[] p[i];
+        }
+        delete[] p;
+    }
+
+};
+
+
+#endif //JAKDOJADE_ARRAY2D_H
