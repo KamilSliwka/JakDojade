9b94c6cb9293ab50f53e7656bd2ee04ddfdc7b5d
KamilSliwka <kamilsliwinski81@gmail.com>
2023-09-07 23:25:38 +0200

    Dijkstra's algorithm that finds the fastest route between cities

--- structures.h
+++ structures.h
@@ -155,6 +155,75 @@ void timeBeetwenCity(Array2D *map, dijkstraCity city, dijkstraCity* arrayCity, i
     }
 }
 
+void dijkstra(int startVertex, int endVertex, Graph* g,bool question, MyString* Names) {
+
+    Queue pq;
+
+    MyVector<int> distances(g->GetnumVertices(), INT_MAX);
+    MyVector<int> parent(g->GetnumVertices(), -1);
+    MyVector<bool> visited(g->GetnumVertices(), false);
+    Vertex w;
+    distances[startVertex] = 0;
+    w.distanceFromStart = 0;
+    w.vertexNumber = startVertex;
+    pq.push(w);
+
+    while (!pq.empty()) {
+        int u = pq.top().vertexNumber;
+        pq.pop();
+
+        if (visited[u]) {
+            continue;
+        }
+
+        visited[u] = true;
+
+        GraphNode* temp = g->adjacencyList[u];
+        while (temp != nullptr) {
+            int v = temp->destination;
+            int cost = temp->cost;
+
+            if (distances[u] != INT_MAX && distances[u] + cost < distances[v]) {
+                distances[v] = distances[u] + cost;
+                parent[v] = u;
+                Vertex tmp;
+                tmp.distanceFromStart = distances[v];
+                tmp.vertexNumber = v;
+                pq.push(tmp);
+            }
+
+            temp = temp->next;
+        }
+    }
+    if (startVertex== endVertex) {
+        cout << 0 << endl;
+    }
+    else if (parent[endVertex] == -1) {
+        cout << "There is no path from vertex " << startVertex << " to vertex " << endVertex << endl;
+    }
+    else if(question==true) {
+        cout<< distances[endVertex]<<" ";
+        int vertex = endVertex;
+        MyVector<int> path;
+        path.push_back(vertex);
+        while (vertex != startVertex) {
+            vertex = parent[vertex];
+            path.push_back(vertex);
+        }
+        for (int i = path.getSize() - 3; i >= 0; i--) {
+            cout << Names[path[i+1]];
+            if (i != 0) {
+                cout << " ";
+            }
+        }
+        cout << endl;
+
+    }
+    else {
+        cout << distances[endVertex] << endl;;
+    }
+}
+
 
 
 
